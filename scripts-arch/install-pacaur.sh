b2f3ae5f3800adbb7351bb48b55e38499d5bf948
Angel Rodriguez <anxolin@gmail.com>
2017-07-20 18:32:12 +0200

    Update pacaur install script

--- scripts-arch/install-pacaur.sh
+++ scripts-arch/install-pacaur.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
 # Install Pacaur
-
 cd /tmp || exit 1
 buildroot="$(mktemp -d)"
+
 sudo -v
+echo "Clean everythin up"
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
-sudo pacman -S --needed --noconfirm base-devel git
+
 mkdir -p "$buildroot"
 cd "$buildroot" || exit 1
+
+echo "Get cower - a simple AUR downloader"
 git clone "https://aur.archlinux.org/cower.git"
+
+echo "Get pacaur - An AUR helper that minimizes user interaction"
 git clone "https://aur.archlinux.org/pacaur.git"
+
+echo "Generate and install cower package (and dependencies)"
 cd "${buildroot}/cower" || exit 1
 makepkg --syncdeps --install --noconfirm
+
+echo "Generate and install pacaur package (and dependencies)"
 cd "${buildroot}/pacaur" || exit 1
 makepkg --syncdeps --install --noconfirm
+
+echo "Clean everythin up"
 cd /tmp || exit 1
 rm -rf "$buildroot"
