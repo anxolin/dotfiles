bdb9cc8dd8f9f7f44b4b6d69ad556efa871654e8
Angel Rodriguez <anxolin@gmail.com>
2018-03-22 00:24:59 +0100

    Remove cmake from install

--- install.sh
+++ install.sh
@@ -71,7 +71,8 @@ install () {
         fi
     # If the platform is OS X, tell the user to install zsh :)
     elif [[ $platform == 'Darwin' ]]; then
-	brew install zsh the_silver_searcher ctags reattach-to-user-namespace cmake
+	brew install zsh the_silver_searcher ctags reattach-to-user-namespace
+  # cmake
         ~/dotfiles/install-vsCode-mac.sh
     fi
 
