cd47d4c8742d22429c03e4950146617bc3c7543f
Angel Rodriguez <anxolin@gmail.com>
2019-05-11 15:52:32 +0200

    Fix path name

--- install/dotfiles_vim.sh
+++ install/dotfiles_vim.sh
@@ -4,7 +4,7 @@ set -e
 
 # Create Vim backupo dir for swap, backup and undo files
 echo "[dotfiles-vim] Creating Vim special dirs: Swap, Backup and Undo"
-mkdir -p ~/.vim/backup_filesd
+mkdir -p ~/.vim/backup_files
 mkdir -p ~/.vim/swap_files
 mkdir -p ~/.vim/undo_files
 
