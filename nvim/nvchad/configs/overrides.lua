76a9f78a2f2dbd65285742a90c925bd52cad611a
Anxo Rodriguez <anxolin@gmail.com>
2025-10-01 19:42:46 +0100

    Add language support

--- nvim/nvchad/configs/overrides.lua
+++ nvim/nvchad/configs/overrides.lua
@@ -2,23 +2,57 @@ local M = {}
 
 M.treesitter = {
   ensure_installed = {
+    -- Core languages
     "vim",
     "lua",
+    "c",
+    "cpp",
+    "python",
+    "rust",
+    "go",
+    "java",
+    
+    -- Web development
     "html",
     "css",
     "javascript",
     "typescript",
     "tsx",
-    "c",
+    "json",
+    "yaml",
+    "toml",
+    
+    -- Markup and documentation
     "markdown",
     "markdown_inline",
+    
+    -- Shell and config files
+    "bash",
+    "dockerfile",
+    "gitignore",
+    "gitcommit",
+    
+    -- Other common formats
+    "solidity",
+    "graphql",
+    "sql",
   },
   indent = {
     enable = true,
-    -- disable = {
-    --   "python"
-    -- },
   },
+  highlight = {
+    enable = true,
+    -- Disable highlighting for languages that don't have parsers
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+  },
+  -- Auto-install missing parsers
+  auto_install = true,
 }
 
 M.mason = {
