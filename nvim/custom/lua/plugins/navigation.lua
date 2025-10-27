a44f9085832c5ba4cd0f0273407f0f95c6a8a682
Anxo Rodriguez <anxolin@gmail.com>
2025-10-27 00:05:02 +0000

    Add navigation

---
+++ nvim/custom/lua/plugins/navigation.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", 
+    },
+    config = function()
+        
+      -- 🗂️ Toggle the file tree with <leader>e and <C-n>
+      vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree" })
+      vim.keymap.set("n", "<C-n>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Neo-tree" })
+    end,
+  }
+  
+}
